6796e026c72fb75394db2d75aa6c65722da0542f
Trey Guy <tguy728@gmail.com>
2019-10-08 18:07:06 +0000

    fixed error in forbidden message

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -4,7 +4,7 @@ class GramsController < ApplicationController
   def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
-    return render_not_found(:gotniffrn) if @gram.user != current_user
+    return render_not_found(:forbidden) if @gram.user != current_user
     @gram.destroy
     redirect_to root_path
   end
@@ -12,8 +12,8 @@ class GramsController < ApplicationController
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
-    return render_not_found(:gotniffrn) if @gram.user != current_user
-    
+    return render_not_found(:forbidden) if @gram.user != current_user
+
     @gram.update_attributes(gram_params)
     if @gram.valid?
       redirect_to root_path
@@ -37,7 +37,7 @@ class GramsController < ApplicationController
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
-    return render_not_found(:gotniffrn) if @gram.user != current_user
+    return render_not_found(:forbidden) if @gram.user != current_user
   end
 
   def create
